9728ac2f3452b69685ddc0b7e892b8d992374216
Liu Cong <liucong@pingcap.com>
2017-08-22 00:53:25 +0800

    Working on loader

--- data-loading/_env.sh
+++ data-loading/_env.sh
@@ -1,1 +1,3 @@
 dbgen_dir=`readlink -f ../tpch-dbgen`
+mysql_bin="mysql -h 127.0.0.1 -P 4000 -u root"
+database="tpch1"
--- data-loading/load.sh
+++ data-loading/load.sh
@@ -1,4 +1,10 @@
 source _env.sh
-source _dbgen.sh
 
+source _dbgen.sh
 dbgen 1
+
+for file in tpch1/*.tbl; do
+	sql="LOAD DATA LOCAL INFILE '$file' INTO TABLE customer FIELDS TERMINATED BY '|'"
+	$mysql_bin --local-infile -D "$database" -e "$sql"
+	break
+done
