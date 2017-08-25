c933be73b06068ba06bda3e43c2bbdf2b8783541
Liu Cong <innerr@gmail.com>
2017-08-26 01:22:52 +0800

    Fix a small bug

--- data-loading/broaden.sh
+++ data-loading/broaden.sh
@@ -1,18 +1,22 @@
 table="$1"
 scale="$2"
 times="$3"
+suffix="$4"
 
 set -eu
 
 source _meta.sh
 
 if [ -z "$times" ]; then
-	echo "usage: <bin> table-name broaden-times" >&2
+	echo "usage: <bin> table-name broaden-times [output-suffix]" >&2
 	exit 1
+fi
+if [ -z "$suffix" ]; then
+	suffix="$times"
 fi
 
 dir="$dbgen_result_dir_prefix""$scale"
-for file in $dir/$table*; do
-	output=${file/\.tbl/$times\.tbl}
+for file in $dir/$table.tbl*; do
+	output=${file/\.tbl/$suffix\.tbl}
 	cat "$file" | python _broaden.py "$times" > "$output"
 done
